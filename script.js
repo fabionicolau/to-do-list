618fd96d9d7b37b5770940340772b61e01bcef5f
fabionicolau <fabionicolaujr@gmail.com>
2021-12-10 18:45:39 -0300

    requisitos 9

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ function listSelected(event) {
   }
 }
 document.addEventListener('click', listSelected);
+
+function completedList(event) {
+  if (event.target.classList.contains('list')) {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }
+}
+document.addEventListener('dblclick', completedList);
+
+// perguntar
+function eraseAll() {
+  const taskList = document.getElementById('lista-tarefas');
+  while (taskList.hasChildNodes) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+const eraseButton = document.getElementById('apaga-tudo');
+eraseButton.addEventListener('click', eraseAll);
+
+// perguntar
+function eraseCompleted() {
+  const taskList = document.getElementById('lista-tarefas');
+  const list = document.getElementsByClassName('list');
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i].classList.contains('completed')) {
+      taskList.removeChild(list[i]);
+    }
+  }
+}
+const eraseCompletedButton = document.getElementById('remover-finalizados');
+eraseCompletedButton.addEventListener('click', eraseCompleted);
