dc6209df0d24a1ecda6177d3d5be53863a70b455
fabionicolau <fabionicolaujr@gmail.com>
2021-12-10 16:29:24 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function createTaskButton() {
   button.addEventListener('click', createTask);
 }
 createTaskButton();
+
+function listSelected(event) {
+  const list = document.getElementsByTagName('li');
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+      list[i].style.backgroundColor = '';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+document.addEventListener('click', listSelected);
